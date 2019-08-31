1b7a1591dc46e907c6c824c3a763687e68871f22
Ketan J <ketanvj@yahoo.com>
2019-08-31 23:02:36 +0530

    First commit for jenkins test

---
+++ src/main/java/test/TestAChar.java
@@ -0,0 +1,22 @@
+package test;
+
+public class TestAChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char a = 120;
+		System.out.println("Value of a is - " + a);
+		
+		int b = 50;
+		double c;
+		c = b;
+		System.out.println("Value of b is - " + b);
+		System.out.println("Value of c is - " + c);
+		double x = 0;
+		double y = 0;
+		double z = x/y;
+		System.out.println(z);
+	}
+
+}
